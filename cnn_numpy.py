b5642ff5dd15f02237a447feaeed0a4c81bea0c3
danoherl <do370@cam.ac.uk>
2025-05-07 14:08:49 +0100

    First commit

---
+++ cnn_numpy.py
@@ -0,0 +1,249 @@
+#!/opt/anaconda3/bin/python
+
+# Loose basis: https://www.coursera.org/learn/convolutional-neural-networks/programming/4xt9A/convolutional-model-step-by-step/
+
+# We need classes for: ConvLayer, PoolLayer, FlattenLayer, FCLayer, SoftMax/Output Layer
+import numpy as np
+import random
+import math
+import timeit
+import time
+
+class ConvLayer:
+    """
+    Class for a convolutional layer of a neural network.
+    """
+    def __init__(self, input, num_filters, filter_dim, pad = 0, stride = 1):
+        self.p = pad
+        self.f = filter_dim
+        self.s = stride
+        self.n_C = num_filters
+        self.input = np.asarray(input)
+        self.W = np.random.random((self.f,self.f,self.input.shape[3],self.n_C))*0.01
+        self.b = np.random.random((1,1,1,self.n_C))
+        
+        """ Initialises a convolutional layer."""
+    
+    def zero_pad(self):
+        """
+        Pad with zeros all images of the dataset X. The padding is applied to the 
+        height and width of an image.
+
+        Arguments:
+        input -- the input to the ConvLayer from the previous layer.
+        
+        Returns:
+        Padded image of shape (m, n_H + 2 * pad, n_W + 2 * pad, n_C)
+        """
+        pad_dims = ((0,0), (self.p,self.p), (self.p, self.p), (0,0))
+        self.input = np.pad(self.input, pad_dims) 
+
+    def single_conv(self, input_slice, current_f):
+        """
+        Perform one convolution step on a given slice and features.
+
+        Arguments:
+        input_slice -- slice of input data of shape (f, f, n_C_prev) 
+        W -- a filter of weight params, shape (f, f, n_C_prev, n_C) 
+        b -- bias parameters in a window, shape (1, 1, 1, n_C)
+
+        Returns:
+        Z -- a scalar, to be put into activation.
+        """
+        Z = np.sum(np.multiply(input_slice, self.W[:,:,:,current_f]))
+        + float(self.b[:,:,:,current_f])
+        return Z
+    
+    def new_shape(self):
+        """
+        Find the output shape, using the input shape.
+
+        Returns:
+        out_h -- number of rows of layer output.
+        out_w -- number of columns of layer output.
+        """
+        out_h = math.floor((self.input.shape[1] + 2 * self.p - self.f)/self.s) + 1
+        out_w = math.floor((self.input.shape[2] + 2 * self.p - self.f)/self.s) + 1
+        return [out_h, out_w]
+    
+    def select_slice(self, i, j, k):
+        """
+        Creates a slice of the input, ready to convolve with the filter.
+        This prepares input for use in single_step function.
+        Arguments:
+        i -- the training example currently in use.
+        j -- the current row of the convolution.
+        k -- the current column of convolution.
+
+        Returns:
+        A slice of the input with dimensions equal to the filter.
+        Dimensions (self.f,self.f, self.input.shape[3])
+
+        """
+        h1 = j*self.s
+        h2 = h1 + self.f
+        w1 = k*self.s
+        w2 = w1 + self.f
+        return self.input[i, h1:h2,w1:w2,:]
+        
+
+    def forward(self):       
+        """
+        Perform a forward pass through one convolutional layer.
+        Returns:
+        Z - the output, to be passed into activation.
+
+        """
+        m = self.input.shape[0]
+        out_h, out_w = self.new_shape()
+        self.zero_pad()
+        Z = np.zeros((m,out_h,out_w, self.n_C))
+        for i in range(m):
+            for j in range(out_h):  
+                for k in range (out_w):
+                    for l in range(self.n_C):
+                        Z[i,j,k,l] = self.single_conv(self.select_slice(i, j, k),l)
+                        
+        return Z
+    
+    def backward(self):
+        """
+        Perform a backward pass through one convolutional layer.
+        """
+        dA = np.zeros(self.input.shape)
+        in_h, in_w = self.input.shape[0],self.input.shape[1]
+
+        return 
+        
+
+class PoolLayer:
+    """
+    Class for a pool layer of a neural network. Can be MAX or AVERAGE pool.
+    """
+    def __init__(self, input, stride, filter_width, mode = "MAX"):
+        self.input = np.asarray(input)
+        self.s = stride
+        self.f = filter_width
+        self.mode = mode
+
+    
+    # Functions: forward, max, average, backward,
+    def select_slice(self, i, j, k, l):
+            """
+            Creates a slice of the input, ready to convolve with the filter.
+            This prepares input for use in single_step function.
+            Arguments:
+            i -- the training example currently in use.
+            j -- the current row of the convolution.
+            k -- the current column of convolution.
+
+            Returns:
+            A slice of the input with dimensions equal to the filter.
+            Dimensions (self.f,self.f, self.input.shape[3])
+
+            """
+            h1 = j * self.s
+            h2 = h1 + self.f
+            w1 = k * self.s
+            w2 = w1 + self.f
+            return self.input[i, h1:h2,w1:w2,l]
+    
+    def single_step(self, input_slice):
+        if self.mode == "MAX":
+            result = np.max(input_slice)
+        elif self.mode == "AVERAGE":
+            result = np.mean(input_slice)
+        return result 
+
+    def new_shape(self):
+        out_h = math.floor((self.input.shape[1] - self.f)/self.s) + 1
+        out_w = math.floor((self.input.shape[2] - self.f)/self.s) + 1
+        return [out_h, out_w]
+        
+        
+
+    def forward(self):
+        """
+        Performs a forward pool.
+        """
+        # How to do this?
+        # Not a filter. Just pass a window and pick the max in each window
+        # So, just take slices, move forward by stride, find max/average in each case
+        m, n_C = self.input.shape[0], self.input.shape[3]
+        out_h, out_w = self.new_shape()
+        Z = np.zeros((m,out_h,out_w, n_C))
+        for i in range(m):
+                    for j in range(out_h):  
+                        for k in range (out_w):
+                            for l in range(n_C):
+                                Z[i,j,k,l] = self.single_step(self.select_slice(i, j, k, l))
+                                
+        return Z
+        
+
+    def backward(self):
+        pass 
+
+class FlattenLayer:
+    """
+    Class for a flatten layer of a neural network.
+    """
+    # This takes a convolution and flattens it out. that is it. so just a reshape 
+    def __init__(self, input):
+        self.input = np.asarray(input)
+
+
+    def forward(self):
+        reshape = (self.input.shape[0], int(self.input.size / self.input.shape[0]))
+        return np.reshape(self.input, reshape)        
+
+    def backward(self):
+        pass
+
+
+class FCLayer:
+    """
+    Class for a fully connected layer of a neural network.
+    """
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    """CONV TESTING"""
+    # x = np.random.random((100,20,20,3))
+    # convtest = ConvLayer(input=x,num_filters=3,filter_dim=3,pad=3,stride=1)
+    # before = time.time()
+    # test = convtest.forward()
+    # after = time.time()
+    # print(f"Time taken: {after - before}")
+    # print(test.shape)
+
+    """POOL TESTING"""
+    # x = np.random.random((1,3,3,3))
+    # pooltest = PoolLayer(input = x, stride = 2, filter_width=2, mode = "AVERAGE")
+    # print(f"input starts as: {pooltest.input}")
+
+    # print(f"And input has shape: {pooltest.input.shape}")
+    # # print(f"So, first dim: {pooltest.input.shape[0]}")
+
+    # before = time.time()
+    # test = pooltest.forward()
+    # after = time.time()
+    # print(f"Our result is: {test}")
+    # print(f"Result has shape: {test.shape}")
+    # # print(f"Time taken: {after - before}")
+
+    """FLATTEN TESTING"""
+    x = np.random.random((1,3,3,3))
+    flattest = FlattenLayer(input=x)
+
+    test = flattest.forward()
+    print(f"Input is {flattest.input}")
+    print(f"And output is {test}")
